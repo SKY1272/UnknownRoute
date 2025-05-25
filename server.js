878ebd78304f9bb71020a5880546151edbc4768e
SKY1272 <MY_NAME@example.com>
2025-05-25 20:39:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
+});
+
+app.post('/products', (req, res) => {
+  res.send('A new product has been added.');
+});
+
+app.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
+});
+
+// Wildcard route for 404
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+

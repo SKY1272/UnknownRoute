d683b8dac112bbe6f75b51560e0893034e3c60c4
SKY1272 <MY_NAME@example.com>
2025-05-26 01:51:12 +0530

    third commit

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express=require('express');
 const app=express();
-const port=5000;
+const port=4000;
 app.use(express.json());
 app.get('/',(req,res)=>{
 res.send('Hello World!')
 });
-app.get('/welcome/:username',(req,res)=>{
-  let username=req.params.username;
-  let role=req.query.role || 'Admin';
-  res.send(`Welcome ${username} your role is ${role}`);
+app.use((req,res,next)=>{
+  console.log(`${req.method} request made for ${req.url}`);
+  next();
 })
-app.get('/welcome/',(req,res)=>{
-  const username=req.query.username || 'Guest';
-  const role=req.query.role || 'User';
-  res.send(`Welcome ${username} your role is ${role}`);
+app.get('/products',(req,res)=>{
+  res.send('Here is the list of all products.')
 })
+app.post('/products',(req,res)=>{
+  res.send('A new Product added.');
+});
+app.get('/catogeries',(req,res)=>{
+    res.send('Here is the list of all catogeries.');
+  });
+  app.post('/catogeries',(req,res)=>{
+    res.send('A new catogery has been added.')
+  })
 app.listen(port,()=>{
   console.log(`Example app listening at http://localhost:${port}`)
 })
